da832cb8600d1128743112b3006720b191dd0b91
Roberto Felipe Bueno Costa <rob_bueno@hotmail.com>
2023-03-26 00:23:54 -0300

    adjustments to production

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const app = express();
 const swaggerUI = require("swagger-ui-express");
 const swaggerFile = require('./swagger/swagger_output.json');
 const bodyParser = require('body-parser');
-var cors = require('cors');
 const port = process.env.PORT || 3000;
 
 require('./startup/logging')();
@@ -12,36 +11,10 @@ require('./startup/routes')(app);
 require('./startup/db')();
 require('./startup/config')();
 require('./startup/validation')();
-
+require('./startup/prod')(app);
 
 app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerFile));
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
-// app.use(cors());
-
-// // Add headers
-// app.use(function (req, res, next) {
-
-
-//     // Website you wish to allow to connect
-//     res.setHeader('Access-Control-Allow-Origin', '*');
-    
-//     // Request methods you wish to allow
-//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    
-//     // Request headers you wish to allow
-//     //res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-//     res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    
-//     // Set to true if you need the website to include cookies in the requests sent
-//     // to the API (e.g. in case you use sessions)
-//     res.setHeader('Access-Control-Allow-Credentials', true);
-
-//     res.setHeader('enablePreflight', true);
-    
-//     // Pass to next layer of middleware
-//     next();
-//     });
 
 app.listen(port, () => winston.info(`Listening on port ${port}...`));
